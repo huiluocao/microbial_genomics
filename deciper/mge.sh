045ac789e575872004d55369ca56c8824df5d918
Huiluo Cao <huiluocao@gmail.com>
2025-04-27 19:02:08 +0800

    Update mge.sh

--- deciper/mge.sh
+++ deciper/mge.sh
@@ -1,3 +1,13 @@
+#### plasmid & virus
 conda activate genomad
 
 genomad end-to-end --cleanup --splits 8 PA_ZSP7002.fna PA_ZSP7002_genomad /ibex/project/c2170/db/genomad_db/ &
+
+#### prophage
+
+
+#### ICEfinder
+
+
+#### ISfinder
+
