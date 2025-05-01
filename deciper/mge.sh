99bb19b20f5a2ce15484851b5ad19e37a93198e3
Huiluo Cao <huiluocao@gmail.com>
2025-05-01 15:03:44 +0800

    Update mge.sh

--- deciper/mge.sh
+++ deciper/mge.sh
@@ -21,6 +21,19 @@ done
 #### phigaro
 
 #### ICEfinder
+1) split gbks to individual records:
+from Bio import SeqIO
+import os,sys
+for file in os.listdir('./'):
+    if file.endswith ('gbk'):
+        with open(file,'r') as f:
+            for rec in SeqIO.parse(f,'genbank'):
+                with open('/ibex/scratch/projects/c2170/pa/gbks/ICEfinder_linux/gbk/%s.gbk'%rec.id,'w') as g:
+                    SeqIO.write(rec,g,'genbank')
+2) run ICEfinder                    
+module load emboss/6.6.0/gnu-6.4.0
+#perl ICEfinder_local.pl bcf_gbk.list
+perl ICEfinder_local.pl bcf_gbk_p6.list
 
 
 #### ISfinder
