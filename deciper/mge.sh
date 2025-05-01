63e4e48e56014653c7460d9777ad95200763ef75
Huiluo Cao <huiluocao@gmail.com>
2025-05-01 08:41:05 +0800

    Update mge.sh

--- deciper/mge.sh
+++ deciper/mge.sh
@@ -4,7 +4,19 @@ conda activate genomad
 genomad end-to-end --cleanup --splits 8 PA_ZSP7002.fna PA_ZSP7002_genomad /ibex/project/c2170/db/genomad_db/ &
 
 #### prophage
+## PHASTEST API
+for file in *.fna
+do
+    a=${file%.fna};
+    wget --post-file=$file "https://phastest.ca/phastest_api?contigs=1" -O $a'_phastest'
+done
 
+for file in *phastest
+do
+    a=$(awk -F'"' '{print $4}' $file);
+    wget "https://phastest.ca/phastest_api?acc=$a" -O ${file}_results
+    echo $a
+done
 
 #### ICEfinder
 
