c16b9542b264939598b29f0f4e7b56043b410b4c
Huiluo Cao <huiluocao@gmail.com>
2025-05-05 07:54:14 +0800

    Update network.sh

--- plasmid/network.sh
+++ plasmid/network.sh
@@ -1,2 +1,14 @@
 https://github.com/labmizrahi/plasmid-segment-dispersal-2024/tree/v1.0
+Zorea, A., Pellow, D., Levin, L. et al. Plasmids in the human gut reveal neutral dispersal 
+and recombination that is overpowered by inflammatory diseases. Nat Commun 15, 3147 (2024). 
+https://doi.org/10.1038/s41467-024-47272-x
+
 https://github.com/macman123/plasmid_network_analysis?tab=readme-ov-file
+	Acman, M., van Dorp, L., Santini, J.M. et al. Large-scale network analysis captures 
+	biological features of bacterial plasmids. Nat Commun 11, 2452 (2020). 
+	https://doi.org/10.1038/s41467-020-16282-w
+ 
+https://github.com/Ecological-Complexity-Lab/Plasmid_multilayer_networks
+The repository contains data and scripts to carry out all analysis in Shapiro et al.,
+"Multilayer Networks of Plasmid Genetic Similarity Reveal Potential Pathways of Gene Transmission" 
+(ISME J 2023, 10.1038/s41396-023-01373-5 https://rdcu.be/c8AK6)
