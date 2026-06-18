4887c287b71f217570dd94b4e2d2ba4dafa7241d
Huiluo Cao <huiluocao@gmail.com>
2026-06-18 19:40:53 +0800

    Create baps.r

---
+++ population_genomics/baps.r
@@ -0,0 +1,8 @@
+install.packages("rhierbaps")
+library(rhierbaps)
+library(ggtree)
+library(ape)
+fasta.file.name <- "data/phylogeny/hku_pm109_parsnp.snp"
+snp.matrix <- load_fasta(fasta.file.name)
+hb.results <- hierBAPS(snp.matrix, max.depth = 2, n.pops = 20)
+hb.results$partition.df
